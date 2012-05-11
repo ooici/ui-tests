93a050d67ba87e9688d63e2f218a7c5e678bd2c3
Marco Moniza <mmoniza@ucsd.edu>
2012-05-11 00:24:36 -0700

    stopped 00A-00B

--- bin/test_rel1_slogin.sh
+++ bin/test_rel1_slogin.sh
@@ -7,11 +7,17 @@ ls generated_tests
 # REMOVE any that's not needed, like the Linux/Windows/etc tests.
 rm generated_tests/*inux*
 rm generated_tests/*indows*
-rm generated_tests/*sample*
-rm generated_tests/*heart*
-rm generated_tests/*orig*
-rm generated_tests/*r1_login*
 #rm generated_tests/*FireFox*
+#rm generated_tests/*OS_X*
+
+#rm generated_tests/*sample*
+#rm generated_tests/*heart*
+#rm generated_tests/*orig*
+#rm generated_tests/*r1_login*
+#rm generated_tests/*00A*
+#rm generated_tests/*00B*
+
+
 
 echo ant jar
 ant jar
