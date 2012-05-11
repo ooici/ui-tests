44c1a72bb514065e3ac0e735b60b7abb7a6c010e
mmoniza <mmoniza@ucsd.edu>
2012-05-11 09:33:16 -0700

    Update rel1_testcases/Test_00A_slogin.java

--- rel1_testcases/Test_00A_slogin.java
+++ rel1_testcases/Test_00A_slogin.java
@@ -40,7 +40,7 @@ public class Test_00A_slogin extends SeleneseTestCase {
 		selenium.waitForPageToLoad("30000");
 		for (int second = 0;; second++) {
 			if (second >= 60) fail("timeout");
-			try { if (selenium.isTextPresent("Showing 1 to")) break; } catch (Exception e) {}
+			try { if (selenium.isTextPresent("Showing 51 to")) break; } catch (Exception e) {}
 			Thread.sleep(1000);
 		}
 
