3866ab48fd5c58a36ae37d5ae601cb8a55c9020f
Marco Moniza <mmoniza@ucsd.edu>
2012-05-12 12:21:34 -0700

    fixed the logout script

---
+++ rel1_testcases/Test_00A_slogout.java
@@ -0,0 +1,44 @@
+package com.example.tests;
+
+import com.thoughtworks.selenium.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import java.util.regex.Pattern;
+
+public class Test_00B_slogout extends SeleneseTestCase {
+	@Before
+	public void setUp() throws Exception {
+		selenium = new DefaultSelenium("localhost", 4444, "*chrome", "http://ion-beta.oceanobservatories.org/");
+		selenium.start();
+	}
+
+	@Test
+	public void test_00B_slogout() throws Exception {
+		selenium.open("https://google.com/");
+		Thread.sleep(15000);
+		for (int second = 0;; second++) {
+			if (second >= 60) fail("timeout");
+			try { if (selenium.isTextPresent("Zoo Tester")) break; } catch (Exception e) {}
+			Thread.sleep(1000);
+		}
+
+		Thread.sleep(15000);
+		selenium.click("id=gbg4");
+		for (int second = 0;; second++) {
+			if (second >= 60) fail("timeout");
+			try { if (selenium.isTextPresent("zootester63@gmail.com")) break; } catch (Exception e) {}
+			Thread.sleep(1000);
+		}
+
+		Thread.sleep(15000);
+		selenium.click("id=gb_71");
+		selenium.waitForPageToLoad("30000");
+		Thread.sleep(15000);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		selenium.stop();
+	}
+}
---
+++ rel1_testcases/Test_0B_slogin.java
@@ -0,0 +1,63 @@
+package com.example.tests;
+
+import com.thoughtworks.selenium.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import java.util.regex.Pattern;
+
+public class Test_00A_slogin extends SeleneseTestCase {
+	@Before
+	public void setUp() throws Exception {
+		selenium = new DefaultSelenium("localhost", 4444, "*chrome", "http://ion-beta.oceanobservatories.org/");
+		selenium.start();
+	}
+
+	@Test
+	public void test_00A_slogin() throws Exception {
+		selenium.open("/");
+		selenium.click("id=login_button");
+		for (int second = 0;; second++) {
+			if (second >= 60) fail("timeout");
+			try { if ("Remember this selection:".equals(selenium.getText("//p[4]/label"))) break; } catch (Exception e) {}
+			Thread.sleep(1000);
+		}
+
+		selenium.select("id=providerId", "label=Google");
+		selenium.uncheck("id=keepidp");
+		selenium.click("id=wayflogonbutton");
+		for (int second = 0;; second++) {
+			if (second >= 60) fail("timeout");
+			try { if (selenium.isTextPresent("for some information from your Google Account.")) break; } catch (Exception e) {}
+			Thread.sleep(1000);
+		}
+
+		selenium.type("id=Email", "U_S_E_R-N_A_M_E");
+		selenium.type("id=Passwd", "P_A_S_S-W_O_R_D");
+		Thread.sleep(1000);
+		selenium.click("id=PersistentCookie");
+		selenium.click("id=signIn");
+		selenium.waitForPageToLoad("30000");
+		for (int second = 0;; second++) {
+			if (second >= 60) fail("timeout");
+			try { if (selenium.isTextPresent("Showing 1 to")) break; } catch (Exception e) {}
+			Thread.sleep(1000);
+		}
+
+		selenium.selectWindow("null");
+		selenium.click("id=logout_link");
+		selenium.waitForPageToLoad("30000");
+		for (int second = 0;; second++) {
+			if (second >= 60) fail("timeout");
+			try { if (selenium.isTextPresent("Getting Started")) break; } catch (Exception e) {}
+			Thread.sleep(1000);
+		}
+
+		Thread.sleep(15000);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		selenium.stop();
+	}
+}
