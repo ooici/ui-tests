be928ee52acce7a6c20a69ed3ad890644b77733f
Marco Moniza <mmoniza@ucsd.edu>
2012-05-12 14:57:58 -0700

    fixed rel2-basic

--- bin/muxer_rel2.pl
+++ bin/muxer_rel2.pl
@@ -2,17 +2,17 @@
 #
 # Site specific customizations section
 my $sg_server = "sg-hub.oceanobservatories.org";
-my $target_base = "http://ion-beta.oceanobservatories.org/";
-#my $target_base = "http://67.58.40.163:3000/";
+my $target_base = "http://67.58.40.163:3000/";
+# my $target_base = "https://buildbot.oceanobservatories.org:9443/ooici-pres-0.1/";
 
 my %browser;
-   $browser{'Windows7'} 		= ['IE', 'Firefox'];
-   $browser{'Windows XP'} 		= ['IE', 'Firefox'];
-   $browser{'Windows Vista'} 	= ['IE', 'Firefox'];
-   $browser{'Linux'} 			= ['Firefox'];
-   $browser{'OS X'} 			= ['Safari', 'Firefox'];
+   $browser{'Windows7'} = ['IE', 'Firefox'];
+   $browser{'Windows XP'} = ['IE', 'Firefox'];
+   $browser{'Windows Vista'} = ['IE', 'Firefox'];
+   $browser{'Linux'} = ['Firefox'];
+   $browser{'OS X'} = ['Safari', 'Firefox'];
 
-   my $generic_password = 'zootester63'; 
+my $generic_password = 'zootester63'; 
    my %account;
    $account{'new_user'}[0]{'name'} = 'zootester63';
    $account{'new_user'}[0]{'password'} = $generic_password;
@@ -30,29 +30,29 @@ foreach $fn (@ARGV) {
   }
   close(FILE);
 
-  for $f (keys %browser ) {
+  for $f (keys %browser ) { 
     for $i ( 0 .. $#{ $browser{$f} } ) {
-      
-      print STDERR "    OUTPUTTING browser specific script for: $browser{$f}[$i] on $f\n";
-      
+
+      print STDERR "    OUTPUTTING browser specific script for: $browser{$f}[$i] on $f    "; 
+
         $ofn = $fn;
-        $ofn =~ s#^rel1_testcases.*/#generated_tests/#;
+        $ofn =~ s#^rel2_testcases.*/#generated_tests/#;
         $ofn =~ s#(\.java)#_$browser{$f}[$i] on $f$1#;
         $ofn =~ s/ /_/g;
         open(OUT, ">$ofn") || die "cannot open outfile $ofn\n";
         my $script_copy = $script;
   
-        # Modify the command to work without selenium grid setu
+        # Modify the command to work without selenium grid setup
         my $platform = "$browser{$f}[$i] on $f";
-        my $sg_server = "sg-hub.oceanobservatories.org";
-        my $target_base = "http://ion-beta.oceanobservatories.org/";
+        # my $sg_server = "sg-hub.oceanobservatories.org";
+        # my $target_base = "https://buildbot.oceanobservatories.org:8080/";
      
         $script_copy =~ s/selenium = new DefaultSelenium\("([^"]+)", 4444, "([^"]+)", "([^"]+)"\);/selenium = new DefaultSelenium("$sg_server", 4444, "$platform", "$target_base");/m;
   
-        # Now edit-in the new username and password 
-        $script_copy =~ s/U_S_E_R-N_A_M_E/$account{'new_user'}[0]{'name'}/mg;
-        $script_copy =~ s/P_A_S_S-W_O_R_D/$account{'new_user'}[0]{'password'}/mg;
-        $script_copy =~ s/A_C_C_O_U_N_T-N_A_M_E/$account{'new_user'}[0]{'account_name'}/mg;
+        # Now edit in the new username and password
+        $script_copy =~ s/U_S_E_R-N_A_M_E/$account{'new_user'}[$try_count]{'name'}/mg;
+        $script_copy =~ s/P_A_S_S-W_O_R_D/$account{'new_user'}[$try_count]{'password'}/mg;
+        $script_copy =~ s/A_C_C_O_U_N_T-N_A_M_E/$account{'new_user'}[$try_count]{'account_name'}/mg;
          
         # if you copy/paste/rename a java file, have to rename the class in script to agree with filename!
         #
@@ -66,9 +66,10 @@ foreach $fn (@ARGV) {
     
         print OUT $script_copy;
         close(OUT);
-        # 0 is valid, so increment it after using it...
+        # 0 is valid, so incriment it after using it...
         $try_count++;
        
     } 
   }
 }
+
--- bin/rel2_basic.sh
+++ bin/rel2_basic.sh
@@ -1,7 +1,7 @@
 rm -rf reports generated_tests
 mkdir reports
 mkdir generated_tests
-bin/multiplex_rel2.pl rel2_testcases/*
+bin/muxer_rel2.pl rel2_testcases/*
 ls generated_tests
 
 # REMOVE ALL BUT THE OSX TESTS
